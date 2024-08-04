9a86a89ef535d091215d8edf32a3c599bbc36c72
Abel Sánchez <abelsandev@gmail.com>
2024-08-04 13:24:25 -0700

    first commit

---
+++ app/src/main/java/com/firebase/firebasekotlin/objetos/ReferenciasFirebase.kt
@@ -0,0 +1,8 @@
+package com.firebase.firebasekotlin.objetos
+
+object ReferenciasFirebase {
+    const val URL_DATABASE = "https://kotlin-58520-default-rtdb.firebaseio.com/"
+    const val DATABASE_NAME = "agenda"
+    const val TABLE_NAME = "contactos"
+}
+
